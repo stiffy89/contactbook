cc40aaa1394a73a341780a2bc4c0748f57f3f0de
Steven Cao <steven.cao@ernox.com>
2023-10-12 02:29:20 +0000

    commit_12/10

--- srv/contactbook-service.js
+++ srv/contactbook-service.js
@@ -1,10 +1,5 @@
 const cds = require('@sap/cds');
-const axios = require('axios');
-const sapcfaxios = require('sap-cf-axios').default;
-const destinationaxios = sapcfaxios("ES5");
-const log = require('cf-nodejs-logging-support');
-log.setLoggingLevel('info');
-log.registerCustomFields(["readData", "updateData"]);
+
 
 module.exports = cds.service.impl(async function (req,res,next) {
 
@@ -45,17 +40,12 @@ module.exports = cds.service.impl(async function (req,res,next) {
 		return req;
 	}
 
-	/* async function readContactsDestination () {
-	   return axios({
+	async function readContactsDestination (sUrl) {
+		return await contactsService.send({
 			method: 'GET',
-			baseURL: 'http://ES5.dest',
-			url: '/sap/opu/odata/iwbep/GWSAMPLE_BASIC/ContactSet',
-			headers: {
-				"content-type": "application/json",
-				'X-CSRF-Token': "fetch"
-			}
+			path: sUrl
 		});
-	} */
+	}
 
 	this.on('DELETE', 'Contacts', async (req, next) => {
 		let data = req.data;
@@ -72,12 +62,11 @@ module.exports = cds.service.impl(async function (req,res,next) {
 
 	this.on('UPDATE', 'Contacts', async (req, next) => {
 		//remap the data coming through to its original format
-		log.info('Updating data');
 		let data = req.data;
 
 		let oOriginalDataObj = {
-			ContactGuid : data.OriginalID,
-			Sex : data.Gender,
+			ContactGuid : data.ContactGuid,
+			Sex : data.Sex,
 			FirstName : data.FirstName,
 			PhoneNumber : data.PhoneNumber,
 			EmailAddress : data.EmailAddress,
@@ -92,7 +81,7 @@ module.exports = cds.service.impl(async function (req,res,next) {
 		};
 		
 
-		let sPath = "/sap/opu/odata/iwbep/GWSAMPLE_BASIC/ContactSet(guid'" + data.OriginalID + "')"; 
+		let sPath = "/sap/opu/odata/iwbep/GWSAMPLE_BASIC/ContactSet(guid'" + data.ContactGuid + "')"; 
 
 		let externalRes = await contactsService.send({
 			method: 'PUT',
@@ -100,59 +89,76 @@ module.exports = cds.service.impl(async function (req,res,next) {
 			data: oOriginalDataObj
 		});
 
-		log.info("data updated", {"updateData":oOriginalDataObj});
-
 		return next();
 
 	});
 
-	
-
 	this.on('EDIT', 'Contacts', async(req, next) => {
-		return next();
-	})
-
-	this.on('NEW', 'Contacts.drafts', async(req, next) => {
-		console.log(req);
-		return next();
-	})
+		return next(); 
+	});
 
 	this.on('READ', 'Contacts.drafts', async(req, next) => {
 		let draftResults = await SELECT.from(Contacts.drafts);
+		let draftAdminResults = await SELECT.from('DRAFT.DraftAdministrativeData');
 		return next();
 	});
 
 	//using external service
 	this.on('READ', 'Contacts', async (req, next) => {
 		//we can actually get the roles of the logged in user here by looking at req.user from the request coming through
-
-		log.info('Reading Data');
-
 		//check to see what the request is and re-map the query
 		let oSelectQuery = req.query.SELECT;
 		let sBaseUrl = "/sap/opu/odata/iwbep/GWSAMPLE_BASIC/ContactSet";
 
-		if (typeof(oSelectQuery.from.ref[0]) == 'object' && oSelectQuery.from.ref[0].id == 'contactbookservice.Contacts') {
+		/* if (req.params.length == 0){
+			//check the select query to see if we have any where clauses
+			if (req.query.SELECT.where){
+
+			} else {
+				sBaseUrl += "?$inlinecount=allpages";
+			}
+		} else {
+			for (var i in req.params){
+				let aKeys = Object.keys(req.params[i]);
+				if (aKeys[0] == 'ContactGuid'){
+					sBaseUrl += ("(guid'" + req.params[i].ContactGuid + "')")
+				}
+			}
+		} */
+
+		let oQuery = req.query.SELECT;
+		if (typeof(oQuery.from.ref[0]) == 'string' && !oQuery.where){
+			sBaseUrl += "?$inlinecount=allpages";
+		}
+		else if (typeof(oQuery.from.ref[0]) == 'object' && !oQuery.where){
+			sBaseUrl += ("(guid'" + oQuery.from.ref[0].where[2].val + "')");
+		}
+		else if (typeof(oQuery.from.ref[0]) == 'string' && oQuery.where) {
+			sBaseUrl += ("(guid'" + oQuery.where[2].val + "')");
+		}
+
+
+
+		
+
+		/* if (typeof(oSelectQuery.from.ref[0]) == 'object' && oSelectQuery.from.ref[0].id == 'contactbookservice.Contacts') {
 			let oWhereClause = oSelectQuery.from.ref[0].where;
 			if (oWhereClause[0].ref[0] == 'ContactGuid' && oWhereClause[1] == '='){
 				sBaseUrl += ("(guid'" + oWhereClause[2].val + "')")
 			}
 		}
 		else if (oSelectQuery.from.ref[0] == 'contactbookservice.Contacts'){
-			sBaseUrl += "?$inlinecount=allpages";
+			
 		}
-		else {
-			return next();
-		}
-
-
 		
 		let externalRes = await contactsService.send({
 			method: 'GET',
 			path: sBaseUrl
-		});
+		}); */
 
-		//return externalRes;
+		let externalRes = await readContactsDestination(sBaseUrl);
+		console.log(externalRes);
+		return externalRes;
 
 		let localRes = await SELECT.from(Contacts);
 
@@ -163,8 +169,7 @@ module.exports = cds.service.impl(async function (req,res,next) {
 				return !(/\d/.test(x.LastName));
 			})
 		}
-
-
+		
 
 		//check to see if we have any new records in this list
 		if (localRes.length == 0) {
@@ -189,7 +194,7 @@ module.exports = cds.service.impl(async function (req,res,next) {
 
 			
 			await srv.create(Contacts).entries(externalData).then(() => {
-				log.info('external data has been read', {'readData':externalData});
+				
 				return next();
 			})
 		} else {
